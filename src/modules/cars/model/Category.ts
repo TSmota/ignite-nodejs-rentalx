4eacfb32f14f1583020a941f24510c5ec5c7f5aa
Thiago Mota <thiagomotasilva2000@gmail.com>
2022-06-11 16:45:17 -0300

    refactor: Move files to cars module folder

---
+++ src/modules/cars/model/Category.ts
@@ -0,0 +1,19 @@
+import { v4 as uuidv4 } from "uuid";
+
+class Category {
+    id: string;
+    name: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
+
+    constructor() {
+        this.id = uuidv4();
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.name = "";
+        this.description = "";
+    }
+}
+
+export { Category };
--- src/modules/cars/model/Specification.ts
+++ src/modules/cars/model/Specification.ts
@@ -1,11 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 
-export class Category {
-    public id: string;
-    public name: string;
-    public description: string;
-    public createdAt: Date;
-    public updatedAt: Date;
+class Specification {
+    id: string;
+    name: string;
+    description: string;
+    createdAt: Date;
+    updatedAt: Date;
 
     constructor() {
         this.id = uuidv4();
@@ -15,3 +15,5 @@ export class Category {
         this.description = "";
     }
 }
+
+export { Specification };
