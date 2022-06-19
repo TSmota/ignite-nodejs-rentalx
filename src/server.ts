29844c93501786dbb1d7c32603e48dd6fdb3d399
Thiago Mota <thiagomotasilva2000@gmail.com>
2022-06-19 17:51:59 -0300

    docs: added Category and Specification routes documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { appRoutes } from "./routes";
+import swaggerDoc from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.use(appRoutes);
 
